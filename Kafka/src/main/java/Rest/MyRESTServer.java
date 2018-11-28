bd5569c70ee51374ea4ae5c5bb6a12fd8779d4b9
Ze Canelha <zecanelha@gmail.com>
2018-11-28 19:12:58 +0000

    Initial commit

---
+++ Kafka/src/main/java/Rest/MyRESTServer.java
@@ -0,0 +1,20 @@
+package Rest;
+
+import java.net.URI;
+
+import javax.ws.rs.core.UriBuilder;
+
+import org.glassfish.jersey.jdkhttp.JdkHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+
+public class MyRESTServer {
+
+ private final static int port = 9998;
+ private final static String host="http://localhost/";
+
+  public static void main(String[] args) {
+  URI baseUri = UriBuilder.fromUri(host).port(port).build();
+  ResourceConfig config = new ResourceConfig(StudentsKeeper.class);
+  JdkHttpServerFactory.createHttpServer(baseUri, config);
+ }
+}
